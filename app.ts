ced3979125387169ca04bef02a5876108e2aab31
MuhammadMomin095 <160387789+MuhammadMomin095@users.noreply.github.com>
2024-02-25 00:57:35 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,8 @@
+let places : string [] = ['multan','islamabad','turkey','swat','kashmir'];
+console.log('original ' + places);
+console.log('copy ' + [...places].sort());
+console.log('original ' + places);
+console.log('copy ' + [...places].sort().reverse());
+console.log('copy ' + [...places].sort().reverse());
+console.log('original :' + places.sort());
+console.log('original ' + places.reverse());
